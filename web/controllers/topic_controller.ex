59114aba8a3870865f05cddeca9396a4c18376cc
Yan P Aung <yan.pye.aung@gmail.com>
2021-07-05 20:56:39 -0700

    Add update topic handler

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -33,4 +33,18 @@ defmodule Discuss.TopicController do
 
     render conn, "edit.html", changeset: changeset, topic: topic
   end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    changeset = Repo.get(Topic, topic_id)
+      |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset } ->
+        render conn, "eidt.html", changeset: changeset
+    end
+  end
 end
