d7a1efdc59997b585c8a1af26f31ea34ef83aa8c
Yan P Aung <yan.pye.aung@gmail.com>
2021-07-22 20:23:40 -0700

    Test out pull and push to and from channel

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,7 +4,7 @@ let socket = new Socket('/socket', { params: { token: window.userToken } });
 
 socket.connect();
 
-let channel = socket.channel('topic:subtopic', {});
+let channel = socket.channel('comments:1', {});
 channel
   .join()
   .receive('ok', (resp) => {
@@ -13,5 +13,9 @@ channel
   .receive('error', (resp) => {
     console.log('Unable to join', resp);
   });
+
+document.querySelector('button').addEventListener('click', function () {
+  channel.push('comment:hello', { hi: 'there' });
+});
 
 export default socket;
