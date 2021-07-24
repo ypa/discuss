888521b68d30efd9be751c848eaf6ca652f30e09
Yan P Aung <yan.pye.aung@gmail.com>
2021-07-23 20:04:22 -0700

    Clean up test code and wire up from client to channel about topic

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -2,14 +2,11 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
+    IO.puts(name)
     {:ok, %{hey: "there"}, socket}
   end
 
   def handle_in(name, message, socket) do
-    IO.puts("++++")
-    IO.puts(name)
-    IO.inspect(message)
-
     {:reply, :ok, socket}
   end
 end
